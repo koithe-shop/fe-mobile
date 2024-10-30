135f86e928b19af7fa3f78924d267e883a5b1dd5
Zane <nhiphm302@gmail.com>
2024-10-30 23:05:06 +0700

    resolved conflict

--- screens/HomeScreen.js
+++ screens/HomeScreen.js
@@ -41,7 +41,7 @@ const HomeScreen = ({ navigation }) => {
         setLoading(false);
       }
      };
-     
+     checkTokenExpiration();
      fetchCategories();
      fetchProducts();
   }, []);
